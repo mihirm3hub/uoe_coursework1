9d1c6b0c27e9296a545402dd3562ea032e3b2572
Mihirs_Legion <mihirmainkarm3@gmail.com>
2024-11-18 23:56:25 +0000

    FOS: 3rd commit

--- Fundamentals_of_Security_ECMM462/Task 2.1/T4/myPKE.py
+++ Fundamentals_of_Security_ECMM462/Task 2.1/T4/myPKE.py
@@ -1,29 +1,27 @@
 import random
 
 def generatePKE(N):
-    # Generate M1 as a random permutation of range 1 to N
+    # Create matrices M1, M2 & M3
     M1 = random.sample(range(1, N + 1), N)
-    
-    # Generate M2 as an NxN matrix where each row is a random permutation of range 1 to N
     M2 = [random.sample(range(1, N + 1), N) for _ in range(N)]
-    
-    # Initialize M3 as an NxN matrix with zeroes
     M3 = [[0] * N for _ in range(N)]
     
-    # Construct M3 to satisfy M3(M2(M1(k), p), k) = p
+    # Construct M3 to related to expression M3(M2(M1(k), p), k) = p
+    # encryption = M2(public_key, p)
+    # decryption M3(encrypted, k) = p
     for k in range(1, N + 1):
         public_key = M1[k - 1]  # M1(k) in 1-indexed terms
         for p in range(1, N + 1):
-            encrypted = M2[public_key - 1][p - 1]  # M2(public_key, p)
-            M3[encrypted - 1][k - 1] = p  # Set M3(encrypted, k) = p
+            encryption = M2[public_key - 1][p - 1]  
+            M3[encryption - 1][k - 1] = p 
     
     return M1, M2, M3
 
 def encrypt(k, p, M1, M2):
-    # Transform the private key k into a public key using M1
+ 
     public_key = M1[k - 1]
-    
-    # Encrypt the plaintext p using M2
+
+    # Encrypt the plaintext p using M2 
     ciphertext = M2[public_key - 1][p - 1]
     return ciphertext
 
@@ -32,7 +30,7 @@ def decrypt(k, ciphertext, M3):
     plaintext = M3[ciphertext - 1][k - 1]
     return plaintext
 
-# Example usage
+# Example
 N = 5
 M1, M2, M3 = generatePKE(N)
 
@@ -47,11 +45,11 @@ print("Ciphertext:", ciphertext)
 decrypted_plaintext = decrypt(private_key, ciphertext, M3)
 print("Decrypted Plaintext:", decrypted_plaintext)
 
-# Printing matrices for reference
-print("\nM1:", M1)
-print("\nM2:")
+# Matrices
+print("M1:", M1)
+print("M2:")
 for row in M2:
     print(row)
-print("\nM3:")
+print("M3:")
 for row in M3:
     print(row)
